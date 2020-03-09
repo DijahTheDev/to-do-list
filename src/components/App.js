a5eeef33fe51314df89203839ea75c8193f11658
khadijah faison <khadijahfaison@gmail.com>
2020-03-08 22:49:06 -0700

    styles done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import App from "../redux/app"
+import Todo from "./Todo.js"
 
 export default props => {
   return (
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,6 +6,8 @@ export default props => {
   const [todo, setTodo] = useState("")
   const inputEl = useRef(null)
   const [view, setView] = useState("all")
+  const Pluralize = ({ count }, item, suffix = "s") =>
+    `${count} ${item}${count !== 1 ? suffix : ""}`
 
   function handleSubmit(e) {
     e.preventDefault()
@@ -20,58 +22,71 @@ export default props => {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          ref={inputEl}
-          type="text"
-          onChange={e => setTodo(e.target.value)}
-          placeholder="Go Shopping..."
-          value={todo}
-        />
-        <button type="submit">Add</button>
-      </form>
-      <ul>
-        {todos.map(todo => (
-          <li
-            key={"todo" + todo.id}
-            className={todo.status === "completed" ? "completed" : ""}
-            onClick={e => toggle(todo.id)}
-          >
-            {todo.text} <button onClick={e => del(todo.id)}>X</button>
-          </li>
-        ))}
-      </ul>
-      <p>Count: {count} items left</p>
+    <div className="container">
       <div>
-        <label htmlFor="all">All</label>
-        <input
-          checked={view === "all" ? true : false}
-          onChange={e => changeView("all")}
-          name="view"
-          id="all"
-          type="radio"
-        />
-        <br />
-        <label htmlFor="active">Active</label>
-        <input
-          checked={view === "active" ? true : false}
-          onChange={e => changeView("active")}
-          name="view"
-          id="active"
-          type="radio"
-        />
-        <br />
-        <label htmlFor="completed">Completed</label>
-        <input
-          checked={view === "completed" ? true : false}
-          onChange={e => changeView("completed")}
-          name="view"
-          id="completed"
-          type="radio"
-        />
+        <h1>To-Do List</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            ref={inputEl}
+            type="text"
+            onChange={e => setTodo(e.target.value)}
+            placeholder="Pay bills..."
+            value={todo}
+            className="inputField"
+          />
+          <button className="addTask" type="submit">
+            Add
+          </button>
+        </form>
+        <ul>
+          {todos.map(todo => (
+            <li
+              key={"todo" + todo.id}
+              className={todo.status === "completed" ? "completed" : ""}
+              onClick={e => toggle(todo.id)}
+            >
+              {todo.text}{" "}
+              <button className="deleteButton" onClick={e => del(todo.id)}>
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+        <div className="footer">
+          <p>{Pluralize({ count }, "item")} left</p>
+          <div className="filter">
+            <label htmlFor="all">All</label>
+            <input
+              checked={view === "all" ? true : false}
+              onChange={e => changeView("all")}
+              name="view"
+              id="all"
+              type="radio"
+            />
+            <br />
+            <label htmlFor="active">Active</label>
+            <input
+              checked={view === "active" ? true : false}
+              onChange={e => changeView("active")}
+              name="view"
+              id="all"
+              type="radio"
+            />
+            <br />
+            <label htmlFor="completed">Completed</label>
+            <input
+              checked={view === "completed" ? true : false}
+              onChange={e => changeView("completed")}
+              name="view"
+              id="all"
+              type="radio"
+            />
+          </div>
+          <button className="clear" onClick={e => clear()}>
+            Clear Completed
+          </button>
+        </div>
       </div>
-      <button onClick={e => clear()}>Clear Completed</button>
     </div>
   )
 }
